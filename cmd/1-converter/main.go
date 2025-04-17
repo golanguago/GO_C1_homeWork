7aad863f2905b200fa81aa96e786bf39b616c8b8
Pavel A. <golanguago@mail.ru>
2025-04-17 05:06:08 +0500

    Lessons and homework

--- cmd/1-converter/main.go
+++ cmd/1-converter/main.go
@@ -3,8 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	const USDtoEUR = 0.88
-	const USDtoRUB = 84.0
-	EURtoRUB := 1 / USDtoEUR * USDtoRUB
-	fmt.Println("1 EUR to RUB = ", EURtoRUB)
+
+	// //hw #1
+	// const USDtoEUR = 0.88
+	// const USDtoRUB = 84.0
+	// EURtoRUB := 1 / USDtoEUR * USDtoRUB
+	// fmt.Println("1 EUR to RUB = ", EURtoRUB)
+
+	// my variant hw #2
+	const convUSDtoEUR = 0.88
+	const convUSDtoRUB = 84.0
+	getUserInput := getUserInput()
+	fmt.Printf("Готово! %.2f EUR = %.2f RUB.", getUserInput, calculate(getUserInput, convUSDtoEUR, convUSDtoRUB))
+}
+
+func getUserInput() float64 {
+	fmt.Println("Введите сумму EUR для конвертации в RUB: ")
+	var EURtoRUB float64
+	fmt.Scan(&EURtoRUB)
+	fmt.Println("Принято, считаем...")
+	return EURtoRUB
+}
+
+func calculate(EURtoRUB float64, USDtoEUR float64, USDtoRUB float64) float64 {
+	convEURtoRUB := EURtoRUB / USDtoEUR * USDtoRUB
+	return convEURtoRUB
 }
--- cmd/inprocess_lesson/main.go
+++ cmd/inprocess_lesson/main.go
@@ -6,9 +6,29 @@ import (
 )
 
 func main() {
-	userHeight := 1.8
-	var userWeight float64 = 100
-	var IMT = userWeight / math.Pow(userHeight, 2)
+	fmt.Println("_____Подсчёт ИМТ_____")
+	IMT := calculate(getIMT())
+	outputResult(IMT)
+}
 
-	fmt.Println(IMT)
+func outputResult(imt float64) {
+	fmt.Println("Ваш индекс массы тела: ")
+	fmt.Printf("%.1f", imt)
+}
+
+func getIMT() (float64, float64) {
+
+	var userHeight float64
+	var userWeight float64
+	fmt.Println("Введите свой рост в сантиметрах: ")
+	fmt.Scan(&userHeight)
+	fmt.Print("Введите свой вес в килограммах: ")
+	fmt.Scan(&userWeight)
+	return userHeight, userWeight
+}
+
+func calculate(userH float64, userW float64) float64 {
+	const IMTPower = 2
+	result := userW / math.Pow(userH/100, IMTPower)
+	return result
 }
