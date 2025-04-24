187d24995adaa42a5781002fd7d7ffeb80d5c3ef
Pavel A. <golanguago@mail.ru>
2025-04-25 01:39:10 +0500

    5.8_lesson

--- cmd/inprocess_lesson/main.go
+++ cmd/inprocess_lesson/main.go
@@ -7,14 +7,22 @@ import (
 
 func main() {
 	fmt.Println("_____Подсчёт ИМТ_____")
-	IMT := calculate(getIMT())
-	outputResult(IMT)
+	for {
+		IMT := calculate(getIMT())
+		fmt.Println(printResult(IMT))
+		userChoice := repeatOrNotCalc()
+		if !userChoice {
+			fmt.Print("Goodbye!")
+			break
+		}
+
+	}
 }
 
-func outputResult(imt float64) {
-	fmt.Println("Ваш индекс массы тела: ")
-	fmt.Printf("%.1f", imt)
-}
+// func outputResult(imt float64) {
+// 	fmt.Println("Ваш индекс массы тела: ")
+// 	fmt.Printf("%.1f", imt)
+// }
 
 func getIMT() (float64, float64) {
 
@@ -31,4 +39,22 @@ func calculate(userH float64, userW float64) float64 {
 	const IMTPower = 2
 	result := userW / math.Pow(userH/100, IMTPower)
 	return result
+}
+
+func printResult(IMT float64) string {
+	if IMT < 16 {
+		return "Очень маленький вес"
+	} else {
+		return "Не самый маленький вес"
+	}
+}
+
+func repeatOrNotCalc() bool {
+	var choice string
+	fmt.Print("Do you want try again? (y/n): ")
+	fmt.Scan(&choice)
+	if choice == "y" || choice == "Y" {
+		return true
+	}
+	return false
 }
